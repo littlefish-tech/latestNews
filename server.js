f89b312c20a80021d7195e54436d8ea59da7f917
Yimeng Yu <yimeng.yu@gmail.com>
2020-02-10 22:58:36 -0800

    update handlebars

--- server.js
+++ server.js
@@ -58,24 +58,24 @@ app.get("/scrape", function(req, res) {
   axios.get("https://www.nytimes.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-console.log(response.data)
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article a").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
+        .children("h2")
         .text();
       
-      result.summary = $(this)
-        .children("a")
-        .text();
+      // result.summary = $(this)
+      //   .children(".summary")
+      //   .text();
 
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      // result.link = $(this)
+      // .children("h2")
+      //   .children("a")
+      //   .attr("href");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
